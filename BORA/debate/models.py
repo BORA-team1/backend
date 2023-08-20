cc6672e35ae7d0b159b1562543c5e2cbf7bc829f
julia-heo <100216331+julia-heo@users.noreply.github.com>
2023-08-20 23:24:05 +0900

    Update models.py

--- BORA/debate/models.py
+++ BORA/debate/models.py
@@ -13,3 +13,5 @@ class Debate(models.Model):
     debate_postsec=models.ForeignKey(PostSec, related_name='debate_postsec',on_delete=models.CASCADE)
     debate_line=models.ForeignKey(Line, related_name='debate_line',on_delete=models.CASCADE)
     link=models.TextField(null=True,blank=True)
+    def __str__(self):
+        return "{}: '{}'의 토론 '{}'".format(self.debate_id,self.debate_postsec.sec_post.title,self.title)
